8345e1d7a275b864c2818737d82d476248e88f8c
Alexey <alexey.zhuravel2004@gmail.com>
2026-01-11 23:27:22 +0100

    Refactor: use dict.get() and simplify spouse linking

--- app/main.py
+++ app/main.py
@@ -10,15 +10,17 @@ class Person:
 
 def create_person_list(people: list) -> list:
     people_list = [
-        Person(name=person["name"], age=person["age"]) for person in people
+        Person(name=person.get("name"), age=person.get("age")) for person in people
     ]
 
     for i, person in enumerate(people):
         current = people_list[i]
         if person.get("wife"):
-            current.wife = Person.people[person["wife"]]
+            wife_name = person.get("wife")
+            current.wife = Person.people[wife_name]
 
         elif person.get("husband"):
-            current.husband = Person.people[person["husband"]]
+            husband_name = person.get("husband")
+            current.husband = Person.people[husband_name]
 
     return people_list
